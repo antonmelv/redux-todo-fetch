7e7bda07f95d1e57d23cc996c359b92ab04c3433
antonmelv <anton.melnyk1723@gmail.com>
2023-04-30 13:50:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addNewTodo, fetchTodos } from './store/todoSlice'
 import './App.css';
+import TodoList from './components/TodoList';
+import InputField from './components/InputField';
 
 function App() {
+  const [text, setText] = useState('');
+  const {status, error} = useSelector(state => state.todos);
+  const dispatch = useDispatch();
+
+  const addTask = () => {
+    dispatch(addNewTodo(text))
+    setText('');
+  };
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputField 
+        text={text}
+        handleInput={setText}
+        handleSubmit={addTask}
+      />
+
+      {status === 'loading' && <h2>Loading...</h2>}
+      {error && <h2>An error occured: {error}</h2>}
+
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import TodoItem from './TodoItem';
+import { useSelector } from 'react-redux';
+
+const TodoList = () => {
+const {todos} = useSelector(state => state.todos);
+
+    return (
+        <ul>
+            {
+                todos.map(todo => 
+                    <TodoItem 
+                    key={todo.id} 
+                    {...todo}
+                    />)
+            }
+        </ul>
+    )
+};
+
+export default TodoList;
